ea5a78eb6f8c5d3b36d8b14058fb1c5c9053fee7
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-09-05 00:04:45 +0300

    Add password models

---
+++ src/main/java/org/com/ivangeorgiev/lockbox/models/PasswordDto.java
@@ -0,0 +1,42 @@
+package org.com.ivangeorgiev.lockbox.models;
+
+import org.com.ivangeorgiev.lockbox.utils.SettingConstants;
+
+public class PasswordDto {
+    private String id;
+    private String title;
+    private String email;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
