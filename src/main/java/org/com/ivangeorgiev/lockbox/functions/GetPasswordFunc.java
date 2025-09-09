85f704c056d12c6a2744c75b723934f3bb121bdd
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-09-09 19:45:27 +0300

    Change to use implement cosmos db settings util class

--- src/main/java/org/com/ivangeorgiev/lockbox/functions/GetPasswordFunc.java
+++ src/main/java/org/com/ivangeorgiev/lockbox/functions/GetPasswordFunc.java
@@ -12,54 +12,50 @@ import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import org.com.ivangeorgiev.lockbox.factory.CosmosDbFactory;
 import org.com.ivangeorgiev.lockbox.factory.CryptographyClientFactory;
+import org.com.ivangeorgiev.lockbox.models.ApiResponseMessage;
 import org.com.ivangeorgiev.lockbox.models.PasswordDto;
+import org.com.ivangeorgiev.lockbox.utils.CosmosDbSettings;
 import org.com.ivangeorgiev.lockbox.utils.KeyVaultSettings;
-import org.com.ivangeorgiev.lockbox.utils.SettingConstants;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Optional;
 
 public class GetPasswordFunc {
-    private final String cosmosDbEndpoint;
-    private final String cosmosDbKey;
-    private final String cosmosDbName;
-    private final String cosmosDbContainerName;
-    private final String cosmosDbPartitionKey;
-
-    public GetPasswordFunc() {
-        cosmosDbEndpoint = System.getenv(SettingConstants.COSMOS_DB_ENDPOINT);
-        cosmosDbKey = System.getenv(SettingConstants.COSMOS_DB_KEY);
-        cosmosDbName = System.getenv(SettingConstants.COSMOS_DB_NAME);
-        cosmosDbContainerName = System.getenv(SettingConstants.COSMOS_DB_CONTAINER_NAME);
-        cosmosDbPartitionKey = System.getenv(SettingConstants.COSMOS_DB_PARTITION_KEY);
-    }
 
     @FunctionName("GetPasswordFunc")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
 
-        String passwordId = request.getQueryParameters()
-                .get("id");
+        String passwordId = request.getQueryParameters().get("id");
 
         if (passwordId == null || passwordId.isEmpty())
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please provide password id").build();
+            return request
+                    .createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponseMessage<>(false, "Please provide password id", null))
+                    .build();
 
         CosmosItemResponse<PasswordDto> itemResponse;
-        try (CosmosDbFactory factory = new CosmosDbFactory(cosmosDbEndpoint, cosmosDbKey)) {
+        try (CosmosDbFactory factory = new CosmosDbFactory(CosmosDbSettings.getInstance().getEndpoint(), CosmosDbSettings.getInstance().getKey())) {
 
-            CosmosContainer container = factory.getContainer(cosmosDbName, cosmosDbContainerName);
+            CosmosContainer container = factory.getContainer(CosmosDbSettings.getInstance().getDatabaseName(), CosmosDbSettings.getInstance().getContainerName());
 
             try {
-                itemResponse = container.readItem(passwordId, new PartitionKey(cosmosDbPartitionKey), PasswordDto.class);
+                itemResponse = container.readItem(passwordId, new PartitionKey(CosmosDbSettings.getInstance().getPartitionKey()), PasswordDto.class);
             } catch (NotFoundException ex) {
-                return request.createResponseBuilder(HttpStatus.valueOf(HttpStatus.NOT_FOUND.value())).body("Item with such id could not be found").build();
+                return request
+                        .createResponseBuilder(HttpStatus.valueOf(HttpStatus.NOT_FOUND.value()))
+                        .body(new ApiResponseMessage<>(false, "Item with such id could not be found", null))
+                        .build();
             }
         }
 
         if (itemResponse.getStatusCode() != HttpStatus.OK.value())
-            return request.createResponseBuilder(HttpStatus.valueOf(itemResponse.getStatusCode())).body("Item with such id could not be found").build();
+            return request
+                    .createResponseBuilder(HttpStatus.valueOf(itemResponse.getStatusCode()))
+                    .body(new ApiResponseMessage<>(false, "Item with such id could not be found", null))
+                    .build();
 
         CryptographyClient cryptoClient = CryptographyClientFactory.create(KeyVaultSettings.getInstance().getKeyId());
 
@@ -68,6 +64,7 @@ public class GetPasswordFunc {
         DecryptResult decryptRes = cryptoClient.decrypt(KeyVaultSettings.getInstance().getEncryptionAlgorithm(), Base64.getDecoder().decode(password.getPassword()));
         password.setPassword(new String(decryptRes.getPlainText(), StandardCharsets.UTF_8));
 
-        return request.createResponseBuilder(HttpStatus.OK).body(password).build();
+        ApiResponseMessage<PasswordDto> apiResponse = new ApiResponseMessage<PasswordDto>(true, "Password created successfully", password);
+        return request.createResponseBuilder(HttpStatus.OK).body(apiResponse).build();
     }
 }
